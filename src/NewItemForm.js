67ba3b89565ae018e97f46ce85d55d8a146c2b5a
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-11-06 15:29:38 -0700

    implemented for state and return value

--- src/NewItemForm.js
+++ src/NewItemForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Item from "./Item"
 
 const NewItemForm = () => {
   
@@ -7,9 +8,49 @@ const NewItemForm = () => {
       data: ""
   }
 
+  const [formData, setFormData] = useState(INITIAL_STATE)  // form state is local to form, values are passed up to parent 
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormData(formData => ({
+      ...formData,
+      [name]: value
+    }))
+  }
+
+  // handle submit form
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  }
+
   return (
-    
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">To-Do: </label>
+      <input
+        id="name"
+        type="text"
+        name="name"
+        placeholder="Add"
+        value={formData.name}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="date">Due By: </label>
+      <input
+        id="date"
+        type="text"
+        name="date"
+        placeholder="Date"
+        value={formData.date}
+        onChange={handleChange}
+      />
+      {/* test if form is working by rendering input text */}
+      {/* <p>{formData.name}</p>
+      <p>{formData.date}</p> */}
+      <button>Add</button>
+    </form>
   );
+
 };
 
 export default NewItemForm;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,19 @@
 import React, {useState} from "react";
 import Item from './Item'
+import NewItemForm from "./NewItemForm";
 
 const TodoList = () => {
     const INITIAL_STATE = [
       { id: 1, name: "Shopping", date: "12/1/20" }
     ]
     const [items, setItems] = useState(INITIAL_STATE);
+    const addItem = (name, date) => {
+      setItems(items => [...items, {name, date}])
+    }
     return (
       <div>
         <h3>To-Do List</h3>
+        <NewItemForm />
         <div>
           {items.map(({id, name, date}) => <Item id={id} name={name} key={id} date={date} />)}
         </div>
