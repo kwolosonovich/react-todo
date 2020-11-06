9d71f0924a37e9ac0ccd6521f8b0bef99fce0725
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-11-06 14:21:28 -0700

    implemented add new item ouline

---
+++ src/NewItemForm.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const NewItemForm = () => {
+  
+  const INITIAL_STATE = {
+      name: "",
+      data: ""
+  }
+
+  return (
+    
+  );
+};
+
+export default NewItemForm;
